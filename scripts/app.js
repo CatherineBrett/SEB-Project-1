92e905cc5c6de28c810a6223b94e0dc049f3f1fd
Catherine Brett <catherinejanebrett@icloud.com>
2024-02-16 18:16:05 +0000

    Adds basic layout, temporary styling, and grid with numbered cells for reference purposes.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const gameGrid = document.querySelector(".game-grid");
+const width = 10;
+const numberOfCells = width * width;
+const gridCells = [];
+
+function makeGrid() {
+  for (let i = 0; i < numberOfCells; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.innerText = i;
+    gameGrid.appendChild(gridCell);
+    gridCells.push(gridCell);
+  }
+}
+
+makeGrid();
